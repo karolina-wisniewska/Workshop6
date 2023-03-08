1e07f8f83aabb07f6d045a9034fe49508483efca
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-03-08 20:45:15 +0100

    get all books

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -1,16 +1,30 @@
 package pl.coderslab.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.coderslab.bean.Book;
+import pl.coderslab.service.MockBookService;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/books")
+//@RequestMapping("/books")
 public class BookController {
 
     @RequestMapping("/helloBook")
     public Book helloBook() {
         return new Book(1L, "9788324631766", "Thinking in Java",
                 "Bruce Eckel", "Helion", "programming");
+    }
+
+    @GetMapping("/books")
+    public List<Book> getAllBooks(MockBookService mockBookService) {
+        return mockBookService.getAllBooks();
+    }
+
+
+    @PostMapping("/books")
+    public void addBook(@RequestBody Book book){
+        // operacje na obiekcie book
+
     }
 }
--- src/main/java/pl/coderslab/service/MockBookService.java
+++ src/main/java/pl/coderslab/service/MockBookService.java
@@ -1,8 +1,33 @@
 package pl.coderslab.service;
 
 import org.springframework.stereotype.Component;
+import pl.coderslab.bean.Book;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class MockBookService {
+    private List<Book> list;
+    private static Long nextId = 4L;
+
+    public MockBookService() {
+
+        list = new ArrayList<>();
+
+        list.add(new Book(1L, "9788324631766", "Thiniking	in	Java", "Bruce	Eckel", "Helion", "programming"));
+
+        list.add(new Book(2L, "9788324627738", "Rusz	glowa	Java.", "Sierra	Kathy,	Bates	Bert", "Helion",
+
+                "programming"));
+
+        list.add(new Book(3L, "9780130819338", "Java	2.	Podstawy", "Cay	Horstmann,	Gary	Cornell", "Helion",
+
+                "programming"));
+    }
+
+    public List<Book> getAllBooks(){
+        return this.list;
+    }
 
 }
